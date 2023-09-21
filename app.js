8d412836e465b32357a1968f5e83cc171f95a410
HaaaamBurger <gemasclashes@gmail.com>
2023-09-21 16:03:10 +0300

    №

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const fs = require('fs/promises');
+const fs = require('fs');
 
 const express = require('express');
 const app = express();
@@ -9,20 +9,7 @@ app.use(express.urlencoded({extended: true}));
 
 const dbPath = path.join(__dirname, 'db.json');
 
-
-const updateDb = async(object) => {
-    await fs.readFile(dbPath, (err, data) => {
-        if (err) throw new Error(err);
-        const users = JSON.parse(data) || [];
-        users.push(object);
-
-        fs.writeFile(dbPath, JSON.stringify(users), (err) => {
-            if (err) throw new Error(err);
-        })
-    })
-}
-
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
     fs.readFile(dbPath, (err, data) => {
         if (err) throw new Error(err);
 
@@ -43,7 +30,7 @@ app.get('/users', (req, res) => {
     })
 })
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = req.body;
     if (user.name.length > 3 && user.age > 0) {
 
@@ -70,7 +57,7 @@ app.post('/users', (req, res) => {
 app.delete('/users/:id', (req, res) => {
     const {id} = req.params;
 
-    fs.readFile(dbPath, (err, data) => {
+    fs.readFile(dbPath, async (err, data) => {
         if (err) throw new Error(err);
 
         let users = JSON.parse(data) || [];
@@ -94,7 +81,7 @@ app.delete('/users/:id', (req, res) => {
 
 app.put('/users/:id', (req, res) => {
     const {id} = req.params;
-    fs.readFile(dbPath, (err, data) => {
+    fs.readFile(dbPath, async (err, data) => {
         if (err) throw new Error(err);
 
         const users = JSON.parse(data);
