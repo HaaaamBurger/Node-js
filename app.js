8fcc4438bcb011bb073e969b859b2b13ea9c5056
HaaaamBurger <gemasclashes@gmail.com>
2023-09-21 15:24:34 +0300

    №

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs/promises');
 
 const express = require('express');
 const app = express();
@@ -10,8 +10,8 @@ app.use(express.urlencoded({extended: true}));
 const dbPath = path.join(__dirname, 'db.json');
 
 
-const updateDb = (object) => {
-    fs.readFile(dbPath, 'utf8', (err, data) => {
+const updateDb = async(object) => {
+    await fs.readFile(dbPath, (err, data) => {
         if (err) throw new Error(err);
         const users = JSON.parse(data) || [];
         users.push(object);
@@ -23,7 +23,7 @@ const updateDb = (object) => {
 }
 
 app.get('/users', (req, res) => {
-    fs.readFile(dbPath, 'utf8', (err, data) => {
+    fs.readFile(dbPath, (err, data) => {
         if (err) throw new Error(err);
 
         if (JSON.parse(data).length) {
@@ -46,8 +46,16 @@ app.get('/users', (req, res) => {
 app.post('/users', (req, res) => {
     const user = req.body;
     if (user.name.length > 3 && user.age > 0) {
-        console.log('Valid')
-        updateDb(user);
+
+        fs.readFile(dbPath, (err, data) => {
+            if (err) throw new Error(err);
+            const users = JSON.parse(data) || [];
+            users.push(user);
+
+            fs.writeFile(dbPath, JSON.stringify(users), (err) => {
+                if (err) throw new Error(err);
+            })
+        })
         res.status(201).json({
             message: `User created!`,
             body: user
