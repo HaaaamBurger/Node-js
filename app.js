0dcd2ff001d9e315208c0dac91b6a3b602442580
HaaaamBurger <gemasclashes@gmail.com>
2023-09-21 15:01:14 +0300

    №

--- app.js
+++ app.js
@@ -9,12 +9,6 @@ app.use(express.urlencoded({extended: true}));
 
 const dbPath = path.join(__dirname, 'db.json');
 
-const usersDb = () => {
-    fs.readFile(dbPath, 'utf8', (err, data) => {
-        if (err) throw new Error(err);
-        console.log(JSON.parse(data));
-    })
-}
 
 const updateDb = (object) => {
     fs.readFile(dbPath, 'utf8', (err, data) => {
@@ -29,15 +23,89 @@ const updateDb = (object) => {
 }
 
 app.get('/users', (req, res) => {
-    res.json(
-        {
+    fs.readFile(dbPath, 'utf8', (err, data) => {
+        if (err) throw new Error(err);
 
+        if (JSON.parse(data).length) {
+            res.json(
+                {
+                    data: JSON.parse(data)
+                }
+            )
+        } else {
+            console.log('No users!');
+            res.json(
+                {
+                    data: 'No Users!'
+                }
+            )
         }
-    )
+    })
 })
 
+app.post('/users', (req, res) => {
+    const user = req.body;
+    if (user.name.length > 3 && user.age > 0) {
+        console.log('Valid')
+        updateDb(user);
+        res.status(201).json({
+            message: `User created!`,
+            body: user
+        })
+    } else {
+        res.status(401).json({
+            message: 'Invalid!'
+        })
+    }
+})
 
+app.delete('/users/:id', (req, res) => {
+    const {id} = req.params;
 
+    fs.readFile(dbPath, (err, data) => {
+        if (err) throw new Error(err);
+
+        let users = JSON.parse(data) || [];
+        if (users.length && users[+id - 1]) {
+            users.splice(+id - 1, 1);
+
+            fs.writeFile(dbPath, JSON.stringify(users), (err) => {
+                if (err) throw new Error(err);
+                res.status(201).json({
+                    message: 'User deleted!'
+                })
+            })
+        } else {
+            res.status(401).json({
+                message: 'Bad request!'
+            })
+        }
+
+    })
+})
+
+app.put('/users/:id', (req, res) => {
+    const {id} = req.params;
+    fs.readFile(dbPath, (err, data) => {
+        if (err) throw new Error(err);
+
+        const users = JSON.parse(data);
+        if (users[+id - 1]) {
+            users[+id - 1] = req.body
+
+            fs.writeFile(dbPath, JSON.stringify(users), (err) => {
+                if (err) throw new Error(err);
+                res.status(201).json({
+                    message: 'User updated!'
+                })
+            })
+        } else {
+            res.status(401).json({
+                message: 'Bad request!'
+            })
+        }
+    })
+})
 
 const PORT = 4444;
 app.listen(PORT, () => {
